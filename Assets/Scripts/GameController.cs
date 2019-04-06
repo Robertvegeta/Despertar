a0f4b5303bfea6e78628fd14f8fb7643879d47dc
Robertvegeta <49247236+Robertvegeta@users.noreply.github.com>
2019-04-07 01:14:38 +0200

    First commit

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+    public Image character;
+
+    private Vector3 targetPosition;
+	// Use this for initialization
+	void Start () {
+        //targetPosition = character.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            //OR- Ray ray = Camera.main.ScreenPointToRay(new Vector3(posX, posY, 0));
+            Vector3 targetPosition = Input.mousePosition;
+            Debug.Log(targetPosition);
+
+        }
+
+       
+        character.transform.position = Vector3.MoveTowards(character.transform.position, targetPosition, Time.deltaTime * 20);
+
+    }
+}
---
+++ Assets/Scripts/hover.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class hover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler
+{
+
+    public Sprite customSprite;
+    public Sprite hoverSprite;
+    public Sprite pressedSprite;
+
+    private Image button;
+
+    public void Start()
+    {
+        button = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        button.sprite = hoverSprite;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        button.sprite = customSprite;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        button.sprite = pressedSprite;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        button.sprite = customSprite;
+    }
+}
